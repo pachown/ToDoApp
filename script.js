ca238ccfc28f284145eafbfde9040977d2d84ccd
Thomas R McNutt <pachown@gmail.com>
2020-02-05 10:47:00 -0700

    Made changes to Javascript
    Still doesn't work fully. Added most of the features

--- script.js
+++ script.js
@@ -1,76 +1,99 @@
-// To-Do
-// make a typeable line that can be turned into local storage
-// make that storage line cross-off-able
-// make that storage line deleteable
-// make the page look good
-// make the page store memory in local storage so that list items appear even after reloading the page
+const todosContainer = document.querySelector('[data-todo]')
+const deleteTasksButton = document.querySelector('[data-delete-all-tasks-button')
+const taskCountElement = document.querySelector('[data-task-count')
+const tasksContainer = document.querySelector('[data-tasks')
+const taskTemplate = document.getElementById('task-template')
+const newTaskForm = document.querySelector('[data-new-task-form')
+const newTaskInput = document.querySelcector('[data-new-task-input')
+const clearCompleteTasksButton = document.querySelector('[data-clear-complete-tasks-button')
 
-var add_todo_btn = document.getElementById('add-btn');
-var todo_input = document.getElementById('todo-input');
-var list = document.getElementById('list');
-add_todo_btn.addEventListener('click', function () {
-    var todo= todo_input.value;
+const LOCAL_STORAGE_TASK_KEY = 'task'
+const LOCAL_STORAGE_SELCECTED_TASK_ID_KEY = task.id
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TASK_KEY)) || []
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELCECTED_TASK_ID_KEY)
 
-    var item = document.createElement('DIV');
-    item.classList.add('item');
+tasksContainer.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'input') {
+        const selectedTask = tasks.find(task => task.id === e.target.id)
+        selectedTask.complete = e.target.checked
+        save()
+        renderTaskCount()
+    }
+})
 
-    var item_text = document.createElement('DIV');
-    item_text.classList.add('item-text');
-    item_text.textContent = todo;
+clearCompletedTasksButton.addEventListener('click', e => {
+    const selectedTask = task.find(task => task.id === selectedTaskId)
+    tasks = tasks.filter(task => !task.complete)
+    saveAndRender()
+})
 
-    var edit_input = document.createElement('INPUT');
-    edit_input.classList.add('edit-input');
-    edit_input.classList.add('hide');
-    edit_input.name = 'edit-input';
-    edit_input.type = 'text';
-    edit_input.value = todo;
+deleteTasksButton.addEventListener('click', e => {
+    tasks = tasks.filter(task => task.id !== selectedTaskId)
+    selectedTaskId = null
+    saveAndRender()
+})
 
-    var edit_input_btn = document.createElement('BUTTON');
-    edit_input_btn.textContent = 'UPDATE';
-    edit_input_btn.classList.add('action-btn');
-    edit_input_btn.classList.add('update-btn');
-    edit_input_btn.classList.add('hide');
-    edit_input_btn.type = 'button';
+newTaskForm.addEventListener('submit', e=> {
+    e.preventDefault()
+    const taskName = newTaskInput.value
+    if (taskName == null || taskName === '') return
+    const task = createTask(taskName)
+    newTaskInput.value = null
+    selectedtasks.push(task)
+    saveAndRender
+})
 
-    var action_btns = document.createElement('DIV');
-    action_btns.classList.add('action-btns');
+function createTask(name) {
+    return {id: Date.now().toString(), name: name, complete: false}
+}
 
-    var edit_btn = document.createElement('BUTTON');
-    edit_btn.classList.add('action-btn');
-    edit_btn.classList.add('edit-btn');
-    edit_btn.textContent = 'EDIT';
+function saveAndRender() {
+    save()
+    render()
+}
 
-    //Edit todos
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_TASK_KEY, JSON.stringify(tasks))
+    localStorage.setItem(LOCAL_STORAGE_SELCECTED_Task_ID_KEY, selectedTaskId)
+}
 
-    edit_btn.addEvenetListener('click', function () {
-        edit_input.classList.remove('hide');
-        item_text.classList.add('hide');
-        edit_input_btn.classList.remove('hide');
-        edit_input_btn.addEventListener('click', function () {
-            item_text.textContent = edit_input.value;
-            edit_input.classList.add('hide');
-            item_text.classList.remove('hide');
-            edit_input_btn.classList.add('hide');
-        });
-    });
-    var remove_btn = document.createElement('BUTTON');
-    remove_btn.classList.add('action-btn');
-    remove_btn.classList.add('remove-btn');
-    remove_btn.textContent = 'REMOVE';
+function render() {
+    clearElement(tasksContainer)
+    renderTasks()
 
-    //Remove Todos
+    const selectedTask = tasks.find(task => task.id === selectedTaskId)
+    if (selectedTaskId = null) {
+        taskDisplayContainer.style.display = 'none'
+    } else {
+        taskDisplayContainer.style.display = ''
+        taskTitleElement.innerText = selectedTask.name
+        renderTaskCount(task)
+    }
+}
 
-    remove_btn.addEventListener('click', function () {
-        item.parentNode.removeChild(item);
-    });
+function renderTasks() {
+    selectedTask.forEach(task => {
+        const taskElement = document.importNode(taskTemplate.content, true)
+        const checkbox = taskElement.querySelector('input')
+        checkbox.id = task.id
+        checkbox.checked = task.complete
+        const label = taskElement.querySelector('label')
+        label.htmlFor = task.id
+        label.append(task.name)
+        tasksContainer.appendChild(taskElement)
+    })
+}
 
-    action_btns.append(edit_input_btn);
-    action_btns.append(edit_btn);
-    action_btns.append(remove_btn);
-    item.append(item_text);
-    item.append(edit_input);
-    item.append(action_btns);
-    list.append(item);
+function renderTaskCount(tasks) {
+    const incompleteTaskCount = tasks.filter(task => !task.complete).length
+    const taskString = incompleteTaskCount === 1 ? "task" : "tasks"
+    taskCountElement.innerText = `${incompleteTaskCount} ${taskString} remaining`
+}
 
-    todo_input.value = '';
-});
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+
+render()
